66365560ad4a878d416061c3ad82e407c73d99bf
stanford kangethe <stanfordkangethe16@gmail.com>
2022-06-08 14:01:18 +0300

    Functions-continued completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(hobby = 'roller-skate') {
+    return `This Saturday, I want to ${hobby}!`
+}
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(param = '*') {
+    return function innerFunc(param1 = 'special'){
+        return `You are ${param}${param1}${param}!`
+    }
+}
+
+console.log(wrapAdjective('%')('a dedicated programmer'));
+
